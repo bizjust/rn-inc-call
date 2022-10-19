73eee642bdf234da8ac62055e56715a93a04ea4a
Ahmad <ahmadj@chromovision.com>
2022-10-19 16:01:24 +0500

    Colored buttons of notification

--- android/src/main/java/com/reactnativefullscreennotificationincomingcall/IncomingCallService.java
+++ android/src/main/java/com/reactnativefullscreennotificationincomingcall/IncomingCallService.java
@@ -14,8 +14,10 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -30,6 +32,7 @@ import androidx.core.content.ContextCompat;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+import android.widget.RemoteViews;
 
 
 public class IncomingCallService extends Service {
@@ -38,6 +41,7 @@ public class IncomingCallService extends Service {
   private String uuid = "";
   private Integer timeoutNumber=0;
   private static final String TAG = "FullscreenSevice";
+  private MediaPlayer player;
   public int onStartCommand(Intent intent, int flags, int startId) {
     String action = intent.getAction();
     if (action != null) {
@@ -81,10 +85,11 @@ public class IncomingCallService extends Service {
   private PendingIntent onButtonNotificationClick(int id, String action) {
     Intent  buttonIntent= new Intent();
     buttonIntent.setAction(action);
+//    player.stop();
     return PendingIntent.getBroadcast(this,id , buttonIntent, PendingIntent.FLAG_IMMUTABLE);
   }
 
-  private Notification buildNotification(Context context, Intent intent) {
+  /* private Notification buildNotification(Context context, Intent intent) {
     Intent fullScreenIntent = new Intent(context, IncomingCallActivity.class);
     Bundle bundle = intent.getExtras();
     fullScreenIntent.putExtra("uuid", uuid);
@@ -141,6 +146,96 @@ public class IncomingCallService extends Service {
         .setFullScreenIntent(fullScreenPendingIntent, true);
     if(bundle.getString("notificationColor")!=null){
       notificationBuilder.setColor(getColorForResourceName(context,bundle.getString("notificationColor")));
+      // notificationBuilder.setColor(ContextCompat.getColor(context, R.color.notification_action_accept));
+    }
+    String iconName = bundle.getString("icon");
+    if (iconName != null) {
+      notificationBuilder.setSmallIcon(getResourceIdForResourceName(context, iconName));
+    }
+    if(timeoutNumber > 0){
+      setTimeOutEndCall(uuid);
+    }
+    return notificationBuilder.build();
+  } */
+
+
+
+  private Notification buildNotification(Context context, Intent intent) {
+    Intent fullScreenIntent = new Intent(context, IncomingCallActivity.class);
+    Bundle bundle = intent.getExtras();
+    fullScreenIntent.putExtra("uuid", uuid);
+    fullScreenIntent.putExtra("name", bundle.getString("name"));
+    fullScreenIntent.putExtra("avatar", bundle.getString("avatar"));
+    fullScreenIntent.putExtra("info", bundle.getString("info"));
+    fullScreenIntent.putExtra("declineText", bundle.getString("declineText"));
+    fullScreenIntent.putExtra("answerText", bundle.getString("answerText"));
+    String channelId=bundle.getString("channelId");
+    fullScreenIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0, fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel notificationChannel=new NotificationChannel(channelId, bundle.getString("channelName"), NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setSound(RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_RINGTONE),
+        new AudioAttributes.Builder()
+          .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+          .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+          .build());
+      notificationChannel.enableLights(true);
+      notificationChannel.enableVibration(true);
+      notificationChannel.setLightColor(Color.WHITE);
+      notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      notificationChannel.setVibrationPattern(new long[] { 0, 1000, 800});
+      notificationManager.createNotificationChannel(notificationChannel);
+    }
+    NotificationCompat.Builder notificationBuilder;
+    RemoteViews notificationLayout = new RemoteViews(getPackageName(), R.layout.activity_call_notify);
+//    Button btn = (Button) findViewById(R.id.btnAccept);
+    notificationLayout.setOnClickPendingIntent(R.id.btnAccept, onButtonNotificationClick(1,Constants.ACTION_PRESS_ANSWER_CALL));
+    notificationLayout.setOnClickPendingIntent(R.id.btnDecline, onButtonNotificationClick(0,Constants.ACTION_PRESS_DECLINE_CALL));
+    notificationLayout.setTextViewText(R.id.txtUser, bundle.getString("name"));
+
+    notificationBuilder = new NotificationCompat.Builder(context,channelId);
+
+//    notificationBuilder.setCustomContentView(notificationLayout);
+//    notificationBuilder.setContentTitle(bundle.getString("name"))
+//        .setContentText(bundle.getString("info"))
+    notificationBuilder.setPriority(NotificationCompat.PRIORITY_MAX)
+        .setCategory(NotificationCompat.CATEGORY_CALL)
+        .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+        .setContentIntent(fullScreenPendingIntent)
+        .setContent(notificationLayout)
+//        .setCustomBigContentView(notificationLayout)
+//         .addAction(
+//           0,
+//           bundle.getString("declineText"),
+//           onButtonNotificationClick(0,Constants.ACTION_PRESS_DECLINE_CALL)
+//        )
+//         .addAction(
+//             0,
+//          bundle.getString("answerText"),
+//               onButtonNotificationClick(1,Constants.ACTION_PRESS_ANSWER_CALL)
+//         )
+      .setAutoCancel(true)
+      .setOngoing(true)
+      .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+      .setVibrate(new long[] { 0, 1000, 800})
+//      .setSound(RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_RINGTONE))
+      .setSound(null)
+        // Use a full-screen intent only for the highest-priority alerts where you
+        // have an associated activity that you would like to launch after the user
+        // interacts with the notification. Also, if your app targets Android 10
+        // or higher, you need to request the USE_FULL_SCREEN_INTENT permission in
+        // order for the platform to invoke this notification.
+        .setFullScreenIntent(fullScreenPendingIntent, true);
+//    Uri notification = RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_RINGTONE);
+//    player = MediaPlayer.create(this, notification);
+//    player.setAudioStreamType(AudioManager.STREAM_ALARM);
+//    player.setLooping(true);
+//    player.start();
+//    channelId.setSound(null, null);
+    if(bundle.getString("notificationColor")!=null){
+      notificationBuilder.setColor(getColorForResourceName(context,bundle.getString("notificationColor")));
+      // notificationBuilder.setColor(ContextCompat.getColor(context, R.color.notification_action_accept));
     }
     String iconName = bundle.getString("icon");
     if (iconName != null) {
@@ -151,6 +246,7 @@ public class IncomingCallService extends Service {
     }
     return notificationBuilder.build();
   }
+
 
   @Override
   public void onCreate() {
